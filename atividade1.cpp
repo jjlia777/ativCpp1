cd219ae21942ed79f3d8cfc561974eb34b12420f
Júlia Rosa <133364820+jjlia777@users.noreply.github.com>
2024-04-12 16:52:09 -0300

    Add files via upload

---
+++ atividade1.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+
+//1 – A empresa TERMAL conhecida pelos seus serviços de instalação de
+//banheiras solicitou um sistema que para somar os serviços feitos pelos seus
+//funcionários.Os serviços são :
+//
+//Instalação – R$300.00
+//Limpeza – R$150.00
+//Manutenção – 499.99
+//Quando o cliente deverá pagar para a empresa ?
+
+
+//int main() {
+//    int inst = 300;
+//    int lpz = 150;
+//    float manu = 499.99;
+//
+//    float soma = inst + lpz + manu;
+//		
+//	std::cout << "Valor total a ser pago:";
+//    std::cout << soma;
+// 
+// return 0;
+//},
+
+
+
+//2 – Joaquim trabalha em uma empresa e recebe R$35.00 reais por hora e
+//trabalha 8 horas por dia.Ele solicitou um programa que calcule quanto ao final
+//de um dia ele deve receber
+
+//int main() {
+//	int horas = 8;
+//	int ganhoHora = 35;
+//
+//	int qtdDia = horas * ganhoHora;
+//	
+// std::cout << "Valor recebido por dia:";
+//	std::cout << qtdDia;
+// 
+// return 0;
+//}
+
+
+
+
+//3 – Cada caixa de bombom vem 27 unidades, Pamela comprou 16 unidades da
+//caixa de bombom, quantos bombons ela tem ?
+
+//int main() {
+//	int uniBombom = 27;
+//	int caixas = 16;
+//
+//	int qtdTotal = uniBombom * caixas;
+//
+//	std::cout << "Quantidade de bombons:";
+//	std::cout << qtdTotal;
+// 
+// return 0;
+//}
+
+
+
+
+//4 – Uma loja vendeu 300 garrafas de água, cada caixa vem 20 garrafas,
+//quantas caixas eles usaram ?
+
+//int main() {
+//	int uniVendidas = 300;
+//	int uniCaixa = 20;
+//
+//	int qtdCaixas = 300 / 20;
+//
+// std::cout << "Caixas usadas:";
+//	std::cout << qtdCaixas;
+//
+// return 0;
+//}
+
+
+
+//5 – Uma loja comprou 120 camisetas e vendeu 80 delas, cada uma custou R$
+//30.00.E logo em seguida comprou mais 50 camisetas, quantas camisetas ele
+//tem agora e qual foi seu faturamento ?
+
+//int main() {
+//	int camisetas = 120;
+//	int camisetasVendidas = 80;
+//	int valorCamiseta = 30;
+//	int camisetasNovas = 50;
+//
+//	int estoque = camisetas - camisetasVendidas;
+//	int valorRecebido = camisetasVendidas * valorCamiseta;
+//
+//	int atuEstoque = estoque + camisetasNovas;
+//	int atuValor = valorRecebido - (camisetasNovas * valorCamiseta);
+//
+//	std::cout << "Camisetas no estoque:";
+//	std::cout << atuEstoque;
+//	std::cout << "\n";
+//
+//	std::cout << "Valor obtido:";
+//	std::cout << atuValor;
+//	std::cout << "\n";
+//
+//	return 0;
+//}
+
+
+
+
